594a1dd65cb0e40551b7e2af4c46670050dd0dd4
WPruitt <pruittstudios@gmail.com>
2014-10-18 00:58:23 -0500

    saving eye's 1 site at a time

---
+++ README.md
@@ -0,0 +1,4 @@
+pruitt.github.io
+================
+
+Where I am tring t turn the internet darker
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Pruitt | I make things darker </title>
+<link rel="shortcut icon" href="">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="inc/css/style.css">
+
+<!--[if IE]>
+<script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body >
+<div class="container-fluid" >
+    <header class="row" >
+       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 header">
+         <nav class="mainnav">
+             <li>
+                 Google
+             </li>
+         </nav>
+       </div>
+    </header>
+</div>
+<p id="navbutton" class="text-center">Pruitt</p>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
+ 
